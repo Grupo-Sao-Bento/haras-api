a72b819daada8d74699bc35789a6e78bf9e43749
rafael-mj <rafaeljakubovsky@gmail.com>
2024-03-06 19:58:24 -0300

    initial commit

---
+++ src/main/java/com/sbgroup/haras/HarasApplication.java
@@ -0,0 +1,13 @@
+package com.sbgroup.haras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbgroup/haras/HarasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbgroup.haras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
