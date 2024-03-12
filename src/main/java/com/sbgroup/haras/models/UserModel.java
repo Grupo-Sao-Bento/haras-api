c19c4519364f36905880d8895e32eee4bdf4b67a
rafael-mj <rafaeljakubovsky@gmail.com>
2024-03-12 03:18:51 -0300

    feat: initialize lombok dependency

--- src/main/java/com/sbgroup/haras/models/UserModel.java
+++ src/main/java/com/sbgroup/haras/models/UserModel.java
@@ -1,10 +1,16 @@
 package com.sbgroup.haras.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "Users")
 public class UserModel implements Serializable {
