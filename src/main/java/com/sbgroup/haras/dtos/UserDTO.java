e194c0bcacbf695f0381b99ded0ae490351b1c29
rafael-mj <rafaeljakubovsky@gmail.com>
2024-03-26 00:36:42 -0300

    refactor: remove size validations from UserDTO

--- src/main/java/com/sbgroup/haras/dtos/UserDTO.java
+++ src/main/java/com/sbgroup/haras/dtos/UserDTO.java
@@ -4,12 +4,11 @@ import com.sbgroup.haras.enums.UserRole;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record UserDTO(
-  @NotBlank @Size(max = 55) String login,
-  @NotBlank @Size(max = 35) String firstName,
-  @NotBlank @Size(max = 125) String lastName,
+  @NotBlank String login,
+  @NotBlank String firstName,
+  @NotBlank String lastName,
   @NotNull UserRole role,
   @NotBlank String password ) {
 }
