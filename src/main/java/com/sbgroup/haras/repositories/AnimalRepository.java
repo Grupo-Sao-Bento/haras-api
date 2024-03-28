a4ec0a0010ef7ed2cfbb80aa1bf56cb4fc342f58
rafael-mj <rafaeljakubovsky@gmail.com>
2024-03-28 00:29:50 -0300

    feat: add AnimalRepository

---
+++ src/main/java/com/sbgroup/haras/repositories/AnimalRepository.java
@@ -0,0 +1,12 @@
+package com.sbgroup.haras.repositories;
+
+import com.sbgroup.haras.models.Animal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, UUID> {
+}
