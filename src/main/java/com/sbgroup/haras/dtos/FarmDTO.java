98363b44a492494c66f97357fd497a0507488180
rafael-mj <rafaeljakubovsky@gmail.com>
2024-04-15 20:07:02 -0300

    feat: add FarmDTO

---
+++ src/main/java/com/sbgroup/haras/dtos/FarmDTO.java
@@ -0,0 +1,16 @@
+package com.sbgroup.haras.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.sql.Timestamp;
+
+public record FarmDTO(
+  @NotBlank String name,
+  @NotBlank String address,
+  @NotBlank String city,
+  @NotBlank String state,
+  @NotBlank String country,
+  @NotNull Timestamp createdAt,
+  @NotBlank String cep
+) {}
